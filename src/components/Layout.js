eef2a36fdd46e6ec330a529ec05470cfca4e8329
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-02-21 21:58:46 +0200

    start task 1

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  padding: 16px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
