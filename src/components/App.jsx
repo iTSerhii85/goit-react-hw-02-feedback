dcc71506961e216aa4775b0d90ad2ceff7ea7a64
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-02-22 17:42:01 +0200

    make folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import { GlobalStyle } from "./GlobalStyle";
 import { Layout } from "./Layout";
 
-import { Section } from "./Section";
-import { Statistics } from "./Statistics";
-import { FeedbackOptions } from "./FeedbackOptions";
+import { Section } from "./Section/Section";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 
 export class App extends React.Component {
   state = {
